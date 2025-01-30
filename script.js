1b68a1589dd54abea5a836a96dd2493ffa6dff3b
Sorosberenc <neisermate@gmail.com>
2025-01-30 20:18:04 +0100

    mobilnezet es icon

---
+++ script.js
@@ -0,0 +1,29 @@
+function rearrangeTable() {
+    const table = document.querySelector("table");
+    const originalRows = Array.from(table.querySelectorAll("tr"));
+
+    table.innerHTML = "";
+
+    if (window.innerWidth <= 768) {
+
+        originalRows.forEach(row => {
+            const cells = Array.from(row.querySelectorAll("td"));
+            cells.forEach(cell => {
+                const newRow = document.createElement("tr");
+                newRow.appendChild(cell);
+                table.appendChild(newRow);
+            });
+        });
+    } else {
+        const newRow = document.createElement("tr");
+        originalRows.forEach(row => {
+            row.querySelectorAll("td").forEach(cell => {
+                newRow.appendChild(cell);
+            });
+        });
+        table.appendChild(newRow);
+    }
+}
+
+window.addEventListener("resize", rearrangeTable);
+window.addEventListener("load", rearrangeTable);
